87b271d47a5d1c20932437c5539b8a39e392b94b
jswhitworth <jswhitworth@gmail.com>
2018-09-28 08:10:42 -0500

    load balance

---
+++ itemp.py
@@ -0,0 +1,20 @@
+resources.append({
+    'name': 'tmpl',
+    'type': 'compute.v1.instanceTemplate',
+    'properties': {
+        'machineType': 'zones/{{ properties["zone"] }}/machineTypes/f1-micro',
+        'disks': [{
+            'deviceName': 'boot',
+            'type': 'PERSISTENT',
+            'boot': True,
+            'autoDelete': True,
+            'initializeParams': {
+                'sourceImage':
+                    'projects/debian-cloud/global/images/family/debian-9'
+            }
+        }],
+        'networkInterfaces': [{
+            'network': 'global/networks/default'
+        }]
+    }
+})
