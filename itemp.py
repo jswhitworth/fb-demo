e100de49ebde7665f8c7fd2e0a18aafc9fe30260
jswhitworth <jswhitworth@gmail.com>
2018-09-28 08:18:58 -0500

    python syntax

--- itemp.py
+++ itemp.py
@@ -1,24 +1,23 @@
 def GenerateConfig(context):
-  """Generate configuration."""
-resources = []
-resources.append({
-    'name': 'tmpl',
-    'type': 'compute.v1.instanceTemplate',
-    'properties': {
-        'machineType': 'zones/{{ properties["zone"] }}/machineTypes/f1-micro',
-        'disks': [{
-            'deviceName': 'boot',
-            'type': 'PERSISTENT',
-            'boot': True,
-            'autoDelete': True,
-            'initializeParams': {
-                'sourceImage':
-                    'projects/debian-cloud/global/images/family/debian-9'
-            }
-        }],
-        'networkInterfaces': [{
-            'network': 'global/networks/default'
-        }]
-    }
-})
-return {'resources': resources}
+    resources = []
+    resources.append({
+        'name': 'tmpl',
+        'type': 'compute.v1.instanceTemplate',
+        'properties': {
+            'machineType': 'zones/{{ properties["zone"] }}/machineTypes/f1-micro',
+            'disks': [{
+                'deviceName': 'boot',
+                'type': 'PERSISTENT',
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage':
+                        'projects/debian-cloud/global/images/family/debian-9'
+                }
+            }],
+            'networkInterfaces': [{
+                'network': 'global/networks/default'
+            }]
+        }
+    })
+    return {'resources': resources}
